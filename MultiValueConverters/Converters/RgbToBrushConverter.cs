a8bffaf82ba892f657769de60cebeedc0b319c0c
Jakob Ferdinand Wegenschimmel <j.wegenschimmel@gmail.com>
2017-04-19 16:17:14 +0200

    MultivalueConverter

---
+++ MultiValueConverters/Converters/RgbToBrushConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace MultiValueConverters.Converters
+{
+    internal class RgbToBrushConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            var red = (byte)(double)values[0];
+            var green = (byte)(double)values[1];
+            var blue = (byte)(double)values[2];
+
+            return new SolidColorBrush(Color.FromRgb(red, green, blue));
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
