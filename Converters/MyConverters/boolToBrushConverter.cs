3c405a09d1b97748848f22cf258134fe2745ec9c
Jakob Ferdinand Wegenschimmel <j.wegenschimmel@gmail.com>
2017-04-19 15:34:37 +0200

    Converters

---
+++ Converters/MyConverters/boolToBrushConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace Converters.MyConverters
+{
+    internal class BoolToBrushConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            //var isChecked = (bool)value;
+
+            //if (isChecked)
+            //    return Brushes.Green;
+
+            //return Brushes.Red;
+            
+            return (bool)value ? Brushes.Green : Brushes.Red;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
