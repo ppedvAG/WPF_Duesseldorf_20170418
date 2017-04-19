fdfa7f00d097156dfb5abb27006f10b4077155b2
Jakob Ferdinand Wegenschimmel <j.wegenschimmel@gmail.com>
2017-04-19 17:00:54 +0200

    Validation

---
+++ Validation/ValidationRules/MustNotBe19ValidationRule.cs
@@ -0,0 +1,18 @@
+﻿using System.Globalization;
+using System.Windows.Controls;
+
+namespace Validation.ValidationRules
+{
+    internal class MustNotBe19ValidationRule : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            var text = value as string;
+
+            if (int.TryParse(text, out int zahl) && zahl != 19)
+                return ValidationResult.ValidResult;
+
+            return new ValidationResult(false, "Zahl darf nicht 19 sein!");
+        }
+    }
+}
