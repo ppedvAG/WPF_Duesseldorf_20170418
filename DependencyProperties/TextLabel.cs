39feeab6bdad66ed023f488c08c1a6fc0da4cd7a
Jakob Ferdinand Wegenschimmel <j.wegenschimmel@gmail.com>
2017-04-20 14:56:40 +0200

    DependencyProperties

---
+++ DependencyProperties/TextLabel.cs
@@ -0,0 +1,87 @@
+﻿using System.Globalization;
+using System.Windows;
+using System.Windows.Media;
+
+namespace DependencyProperties
+{
+    public class TextLabel : FrameworkElement
+    {
+        public static readonly DependencyProperty FontSizeProperty;
+        public static readonly DependencyProperty TextProperty;
+        public static readonly DependencyProperty ForegroundProperty;
+
+        static TextLabel()
+        {
+            var metadata = new FrameworkPropertyMetadata(
+                defaultValue: 11.0,
+                flags: FrameworkPropertyMetadataOptions.AffectsMeasure);
+
+            FontSizeProperty = DependencyProperty.Register(
+                name: nameof(FontSize),
+                propertyType: typeof(double),
+                ownerType: typeof(TextLabel),
+                typeMetadata: metadata,
+                validateValueCallback: v => (double)v > 0);
+
+            TextProperty = DependencyProperty.Register(
+                name: nameof(Text),
+                propertyType: typeof(string),
+                ownerType: typeof(TextLabel),
+                typeMetadata: new FrameworkPropertyMetadata(
+                    defaultValue: string.Empty,
+                    flags: FrameworkPropertyMetadataOptions.AffectsMeasure | 
+                           FrameworkPropertyMetadataOptions.AffectsRender));
+
+            ForegroundProperty = DependencyProperty.Register(
+                name: nameof(Foreground),
+                propertyType: typeof(Brush),
+                ownerType: typeof(TextLabel),
+                typeMetadata: new FrameworkPropertyMetadata(
+                    defaultValue: Brushes.Black,
+                    flags: FrameworkPropertyMetadataOptions.AffectsRender));
+        }
+
+        public double FontSize
+        {
+            get => (double)GetValue(FontSizeProperty);
+            set => SetValue(FontSizeProperty, value);
+        }
+
+        public string Text
+        {
+            get => (string)GetValue(TextProperty);
+            set => SetValue(TextProperty, value);
+        }
+
+        public Brush Foreground
+        {
+            get => (Brush)GetValue(ForegroundProperty);
+            set => SetValue(ForegroundProperty, value);
+        }
+
+        protected override void OnRender(DrawingContext drawingContext)
+        {
+            base.OnRender(drawingContext);
+
+            drawingContext.DrawRectangle(Brushes.LightGray, null, new Rect(RenderSize));
+            drawingContext.DrawText(GetFormattedText(), new Point(2.5, 2.5));
+        }
+
+        protected override Size MeasureOverride(Size availableSize)
+        {
+            var text = GetFormattedText();
+            return new Size(text.Width + 5, text.Height + 5);
+        }
+
+        private FormattedText GetFormattedText()
+        {
+            return new FormattedText(
+                Text,
+                CultureInfo.InvariantCulture,
+                FlowDirection.LeftToRight,
+                new Typeface("Arial"),
+                FontSize,
+                Foreground);
+        }
+    }
+}
