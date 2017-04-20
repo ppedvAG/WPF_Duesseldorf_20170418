7b9ef8f2873c5167a5be3f5858e131dfd3194a98
Jakob Ferdinand Wegenschimmel <j.wegenschimmel@gmail.com>
2017-04-20 15:42:28 +0200

    UserControls

---
+++ OwnUserControls/LoginView.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace OwnUserControls
+{
+    /// <summary>
+    /// Interaction logic for LoginView.xaml
+    /// </summary>
+    public partial class LoginView : UserControl
+    {
+        public LoginView()
+        {
+            InitializeComponent();
+        }
+
+        public string Username
+        {
+            get { return UsernameTextBox.Text; }
+            set { UsernameTextBox.Text = value; }
+        }
+
+        public string Password
+        {
+            get { return PasswordPasswordBox.Password; }
+            set { PasswordPasswordBox.Password = value; }
+        }
+
+        public event RoutedEventHandler LoginClicked
+        {
+            add { LoginButton.Click += value; }
+            remove { LoginButton.Click -= value; }
+        }
+    }
+}
---
+++ OwnUserControls/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace OwnUserControls
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginView_LoginClicked(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show($"Username: {loginView.Username} - Password: {loginView.Password}");
+        }
+    }
+}
